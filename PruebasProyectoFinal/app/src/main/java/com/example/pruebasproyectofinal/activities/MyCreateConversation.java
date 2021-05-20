b57b07aa4bb8d5f54f7110493d80fd6e55fb1073
Alvaro <alvaroortizfalcon@outlook.es>
2021-05-20 20:28:16 +0200

    Cliente, servidor e interfaz sin relacionar entre sí. Interfaz a medias

---
+++ PruebasProyectoFinal/app/src/main/java/com/example/pruebasproyectofinal/activities/MyCreateConversation.java
@@ -0,0 +1,16 @@
+package com.example.pruebasproyectofinal.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pruebasproyectofinal.R;
+
+public class MyCreateConversation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_convesation);
+    }
+}
